e7fee0e6b7d8ce296c34b9345eeced8dcbdc63c7
Javier <djavix.17@gmail.com>
2015-12-08 17:39:10 -0430

    Subiendo estructura del proyecto

---
+++ src/MusicBlog/Comun/Entidades/Entidad.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comun.Entidades
+{
+    public abstract class Entidad
+    {
+        private int _id;
+
+        public Entidad()
+        {
+        }
+
+        public virtual int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+    }
+}
