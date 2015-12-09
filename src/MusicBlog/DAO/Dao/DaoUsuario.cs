190564f8a5705f0a1ab8dd2c0850b366508f43ed
Javier <djavix.17@gmail.com>
2015-12-09 17:15:38 -0430

    Subiendo framework para pruebas unitarias y dao usuario

---
+++ src/MusicBlog/DAO/Dao/DaoUsuario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Comun.Entidades;
+using DAO.IDao;
+
+namespace DAO.Dao
+{
+    public class DaoUsuario : DaoBase<Usuario>, IDaoUsuario
+    {
+    }
+}
