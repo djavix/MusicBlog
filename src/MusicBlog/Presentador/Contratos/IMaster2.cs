a1b27ff610d7b73a2c2edb7da42773f4e5f496ae
Javier <djavix.17@gmail.com>
2016-04-01 17:02:00 -0430

    Actualizacion

---
+++ src/MusicBlog/Presentador/Contratos/IMaster2.cs
@@ -0,0 +1,14 @@
+﻿using Comun.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentador.Contratos
+{
+    public interface IMaster2
+    {
+        IList<Menu> Menu { set; }
+    }
+}
--- src/MusicBlog/Vista/MusicBlog2.master.cs
+++ src/MusicBlog/Vista/MusicBlog2.master.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Presentador.Contratos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,10 +8,33 @@ using System.Web.UI.WebControls;
 
 namespace Vista
 {
-	public partial class MusicBlog2 : System.Web.UI.MasterPage
+	public partial class MusicBlog2 : System.Web.UI.MasterPage, IMaster2
 	{
 		protected void Page_Load(object sender, EventArgs e)
 		{
 		}
-	}
+
+        IList<Comun.Entidades.Menu> IMaster2.Menu
+        {
+            set 
+            {
+                if (value != null  && value.Count > 0)
+                {
+                    foreach(Comun.Entidades.Menu item in value)
+                    {
+                        MenuItem menu = new MenuItem();
+                        menu.Value = item.Id;
+                        menu.Text = item.Nombre;
+                        menu.NavigateUrl = item.Url;
+
+                        Menu.Items.Add(menu);
+                    }
+                }
+                else
+                {
+                    Menu.Visible = false;
+                }
+            }
+        }
+    }
 }
